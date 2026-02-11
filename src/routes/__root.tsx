15fe13c89822ac1848a0a28ef844107f629d33e0
CalebLovell <caleblovell1@gmail.com>
2026-02-11 02:08:30 -0600

    fix meta

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -8,22 +8,71 @@ export const Route = createRootRoute({
 	notFoundComponent: () => <p>Page not found</p>,
 	head: () => ({
 		meta: [
+			{ charSet: "utf-8" },
+			{ name: "viewport", content: "width=device-width, initial-scale=1" },
+			{ title: "Latin American Political History Map" },
 			{
-				charSet: "utf-8",
+				name: "description",
+				content: "A visual timeline of the political history of Latin America",
+			},
+			{ property: "og:title", content: "Latin American Political History Map" },
+			{
+				property: "og:description",
+				content: "A visual timeline of the political history of Latin America",
+			},
+			{ property: "og:url", content: "https://latamap.com" },
+			{
+				property: "og:site_name",
+				content: "Latin American Political History Map",
+			},
+			{ property: "og:type", content: "website" },
+			{
+				property: "og:image",
+				content: "https://latamap.com/images/screenshot.png",
+			},
+			{ property: "og:image:width", content: "1200" },
+			{ property: "og:image:height", content: "630" },
+			{ property: "og:image:type", content: "image/png" },
+			{ name: "twitter:card", content: "summary_large_image" },
+			{ name: "twitter:site", content: "@CalebLovell" },
+			{
+				name: "twitter:title",
+				content: "Latin American Political History Map",
 			},
 			{
-				name: "viewport",
-				content: "width=device-width, initial-scale=1",
+				name: "twitter:image",
+				content: "https://latamap.com/images/screenshot.png",
 			},
 			{
-				title: "TanStack Start Starter",
+				name: "twitter:description",
+				content: "A visual timeline of the political history of Latin America",
+			},
+			{
+				name: "twitter:image:alt",
+				content: "A screenshot of the map set to January 1st, 2023",
 			},
 		],
 		links: [
+			{ rel: "stylesheet", href: appCss },
+			{ rel: "icon", href: "/favicon.ico", type: "image/x-icon" },
 			{
-				rel: "stylesheet",
-				href: appCss,
+				rel: "icon",
+				href: "/favicon-16x16.png",
+				type: "image/png",
+				sizes: "16x16",
 			},
+			{
+				rel: "icon",
+				href: "/favicon-32x32.png",
+				type: "image/png",
+				sizes: "32x32",
+			},
+			{
+				rel: "apple-touch-icon",
+				href: "/apple-touch-icon.png",
+				sizes: "180x180",
+			},
+			{ rel: "manifest", href: "/manifest.json" },
 		],
 	}),
 	shellComponent: RootDocument,
